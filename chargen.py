11634dab37aea66abfb9cc2ac9ca0eacd3fb2ecb
Omer Golan Joel <golan2072@gmail.com>
2018-04-05 21:24:53 +0300

    v0.02

--- chargen.py
+++ chargen.py
@@ -1,7 +1,7 @@
 """
 chargen.py
 Classic Traveller character generator
-v0.01, March 30th, 2018
+v0.02, April 5th, 2018
 By Omer Golan-Joel, golan2072@gmail.com
 This code is open-source
 """
@@ -17,18 +17,6 @@ import stellagama
 
 #career data
 
-Navy={"enlistment": 8, "enlistment DM+1": 3, "enlistment DM+1 level": 8,  "enlistment DM+2": 4,
-"enlistment DM+2 level": 9, "survival": 5, "survival DM+1": 3, "survival DM+1 level": 7,
-"commission": 10, "commission DM+1": 5, "commission DM+1 level": 9, "promotion": 8,
-"promotion DM+1": 4, "promotion DM+1 level": 8, "reenlist": 6, "ranks": ["Ensign", "Lieutenant",
-"Lt Commander", "Commander", "Captain", "Admiral"], "muster": ["Low Passage", "+1 INT", "+2 EDU",
-"Blade", "TAS", "High Passage", "+2 SOC"], "cash": [1000, 5000, 5000, 10000, 20000, 50000, 50000],
-"personal": ["+1 STR", "+1 DEX", "+1 END", "+1 INT", "+1 EDU", "+1 SOC"], "service": ["Ship's Boat",
-"Vacc Suit", "Forward Obs", "Gunnery", "Blade Combat", "Gun Combat"], "advanced": ["Vacc Suit",
-"Mechanical", "Electronics", "Engineering", "Gunnery", "J-o-T"], "advanced 2": ["Medical", "Navigation",
-"Engineering", "Computer", "Pilot", "Admin"], "rank skill key": [5, 6], "rank skills": ["+1 SOC",
-"+1 SOC"]}
-
 #additional data
 
 guns=["Body Pistol", "Autopistol", "Revolver", "Carbine", "Rifle", "Autorifle", "Shotgun", "SMG", "Laser Carbine", "Laser Rifle"]
@@ -39,9 +27,6 @@ vehicles=["Aircraft (Helicopter)", "Aircraft (Propeller-driven)", "Aircraft (Jet
 
 # chardict={"UPP": [], "name": "", "sex": "", "age": 18, "career": "", "rank": 0, "terms": 0, "alive": "",
 # "skills": {}, "possessions": {}, "cash": 0}
-
-testchar={"UPP": [999999], "name": "", "sex": "", "age": 18, "career": "Navy", "rank": 0, "terms": 0, "alive": "",
-"skills": {"Gunnery": 1, "Gun Combat": 1, "Pilot": 1}, "possessions": {}, "cash": 0}
 
 #functions
 
@@ -69,26 +54,26 @@ def name_gen(sex): #input character sex
 	else:	#in case of wrong input
 		return "Tokay" #output placeholder
 
-def add_skill(character, skill): #inputs the character dictionary and skill
+def add_skill(skill_list, upp, skill): #inputs the skill list, UPP, and skill
 	"""
 	adds a skill or characteristic bonus to a character
 	"""
-	if skil=="+1 STR":
-		character["UPP"][0]+=1
-	elif skil=="+1 DEX":
-		character["UPP"][1]+=1
-	elif skil=="+1 END":
-		character["UPP"][2]+=1
-	elif skil=="+1 INT":
-		character["UPP"][3]+=1
-	elif skil=="+1 EDU":
-		character["UPP"][4]+=1
-	elif skil=="+1 SOC":
-		character["UPP"][5]+=1
-	elif skill in character["skills"]:
-		character["skills"][skill] += 1
-	elif skill not in character ["skills"]:
-		character["skills"][skill] = 1
+	if skill=="+1 STR":
+		upp[0]+=1
+	elif skill=="+1 DEX":
+		upp[1]+=1
+	elif skill=="+1 END":
+		upp[2]+=1
+	elif skill=="+1 INT":
+		upp[3]+=1
+	elif skill=="+1 EDU":
+		upp[4]+=1
+	elif skill=="+1 SOC":
+		upp[5]+=1
+	elif skill in skill_list:
+		skill_list[skill] += 1
+	elif skill not in skill_list:
+		skill_list[skill] = 1
 	return character #outputs the character dictionary
 
 def skill_stringer(input_dict): #input a dictionary
@@ -112,67 +97,45 @@ def upp_stringer(input_list): #input a characteristics list
 		output_list.append(str(stellagama.pseudo_hex(item)))
 	return ''.join (output_list) #output a string
 
-def career_choice (character): #input character dictionary
-	if character["UPP"][4]==max(character["UPP"]):
-		character["career"]="Navy"
-	elif character["UPP"][0]==max(character["UPP"]):
-		character["career"]=stellagama.random_choice(["Scout", "Marines"])
-	elif character["UPP"][2]==max(character["UPP"]):
-		character["career"]="Army"
-	elif character["UPP"][3]==max(character["UPP"]):
-		character["career"]="Merchants"
-	else:
-		character["career"]="Other"
-	return character
-
-# def enlistment (chardict): #input character dictionary
-	# career=chardict["career"]
-	# enlisted = stellagama.dice(2, 6)
-	# if chardict["UPP"][career["enlistment DM+1"]]>=career["enlistment DM+1 level"]:
-		# enlisted+=1
-	# if chardict["UPP"][career["enlistment DM+2"]]>=career["enlistment DM+2 level"]:
-		# enlisted+=2
-	# if enlisted >= enlisted_level:
-		# return True
-	# else:
-		# return False
-		
-def chargen (chardict): #input character dictionary
+def career_choice (upp): #input upp list
 	"""
-	main character generation loop
-	"""
-	chardict = career_choice (chardict)
-	career= Navy
-	enlistment=stellagama.dice (2, 6)
-	if enlistment >= career["enlistment"]:
-		chardict["career"]=chardict["career"]
+	chooses a career based on UPP characteristics.
+	"""	
+	if upp[4]==max(upp):
+		career="Navy"
+	elif upp[0]==max(upp):
+		career=stellagama.random_choice(["Scout", "Marines"])
+	elif upp[2]==max(upp):
+		career="Army"
+	elif upp[3]==max(upp):
+		career="Merchants"
 	else:
-		chardict["career"]=stellagama.random_choice(["Navy", "Marines", "Army", "Scouts", "Merchants", "Other"])
-	return chardict
+		career="Other"
+	return career #outputs the chatacter's career
 	
 #classes	
 
 class character:
+	"""character generation class"""
 	def __init__ (self):
-		character.chardict={"UPP": [stellagama.dice(2, 6), stellagama.dice(2, 6), stellagama.dice(2, 6),
-		stellagama.dice(2, 6), stellagama.dice(2, 6), stellagama.dice(2, 6)], "name": "", "sex": sex_gen(),
-		"age": 18, "career": "", "rank": 0, "terms": 0, "alive": "", "skills": {}, "possessions": {},
-		"cash": 0}
-		character.chardict["name"] = name_gen(character.chardict["sex"])
-		character.chardict = chargen (character.chardict)
+		"""generate basic stats"""
+		self.upp=[stellagama.dice(2, 6), stellagama.dice(2, 6), stellagama.dice(2, 6), stellagama.dice(2, 6), stellagama.dice(2, 6), stellagama.dice(2, 6)]
+		self.skills={}
+		self.possessions={}
+		self.rank=0
+		self.terms=0
+		self.sex=sex_gen()
+		self.name=name_gen(self.sex)
+		self.career=career_choice(self.upp)
+		self.age=18
+		"""enlistment"""
 
-#test area
-# career=navy
-# if stellagama.dice (2, 6) >= career["survival"]:
-	# print ("Survived!")
-# else:
-	# print ("Died in the line of duty!")
-	
+#main program	
 character1=character()
-print (upp_stringer(character1.chardict["UPP"]))
-print (character.chardict["sex"])
-print (character.chardict["name"])
-print (character.chardict["career"])
-print (possession_stringer ({"Laser Rifle":1, "Laser Pistol":2, "Low Passage": 3}))
+print (upp_stringer(character1.upp))
+print (character1.sex)
+print (character1.name)
+print (character1.career)
+print (character1.age)
 
 # print (enlistment(testchar))
